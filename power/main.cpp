4a0924b47aa11fa9688596eed5652af814238a9d
sjllls <2012ji123@gmail.com>
2019-04-29 21:56:08 +0800

    Revert PowerHAL

--- power/main.cpp
+++ power/main.cpp
@@ -1,6 +1,5 @@
 /*
- * Copyright (C) 2017 The Android Open Source Project
- * Copyright (C) 2017-2018 The LineageOS Project
+ * Copyright (C) 2016-2019 AngeloGioacchino Del Regno <kholk11@gmail.com>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,17 +14,17 @@
  * limitations under the License.
  */
 
-#define LOG_TAG "android.hardware.power@1.1-service-qti"
-
-// #define LOG_NDEBUG 0
+#define LOG_TAG "RQBalance-PowerHAL"
 
 #include <android/log.h>
 #include <hidl/HidlTransportSupport.h>
-#include <hardware/power.h>
-#ifdef ARCH_ARM_32
-#include <hwbinder/ProcessState.h>
-#endif
+
+#include "common.h"
+#include "RQBalanceHALExt.h"
+#include "Hints.h"
 #include "Power.h"
+
+#define UNUSED __attribute__((unused))
 
 using android::sp;
 using android::status_t;
@@ -36,41 +35,31 @@ using android::hardware::configureRpcThreadpool;
 using android::hardware::joinRpcThreadpool;
 
 // Generated HIDL files
-using android::hardware::power::V1_1::IPower;
-using android::hardware::power::V1_1::implementation::Power;
+using android::hardware::power::V1_3::IPower;
+using android::hardware::power::V1_3::implementation::Power;
 
-int main() {
-#ifdef ARCH_ARM_32
-    android::hardware::ProcessState::initWithMmapSize((size_t)16384);
-#endif
+int main(int argc UNUSED, char** argv UNUSED)
+{
+    ALOGI("Initializing RQBalance-PowerHAL...");
 
-    status_t status;
-    android::sp<IPower> service = nullptr;
-
-    ALOGI("Power HAL Service 1.1 for QCOM is starting.");
-
-    service = new Power();
+    android::sp<IPower> service = new Power();
     if (service == nullptr) {
         ALOGE("Can not create an instance of Power HAL Iface, exiting.");
-
-        goto shutdown;
+        return 1;
     }
-
     configureRpcThreadpool(1, true /*callerWillJoin*/);
 
-    status = service->registerAsService();
+    status_t status = service->registerAsService();
     if (status != OK) {
-        ALOGE("Could not register service for Power HAL Iface (%d).", status);
-        goto shutdown;
+        ALOGE("Could not register service for Power HAL Iface (%d), exiting.", status);
+        return 1;
     }
 
-    ALOGI("Power Service is ready");
+    ALOGI("HIDL Service started.");
+
     joinRpcThreadpool();
-    //Should not pass this line
 
-shutdown:
     // In normal operation, we don't expect the thread pool to exit
-
     ALOGE("Power Service is shutting down");
     return 1;
 }
