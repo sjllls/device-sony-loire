b9383fe29f8fec86fc61264cfcffc4da739ea465
sjllls <2012ji123@gmail.com>
2019-05-03 17:23:56 +0800

    Move to device-sony-common

--- platform.mk
+++ platform.mk
@@ -77,8 +77,7 @@ PRODUCT_PACKAGES += \
     
 # Power
 PRODUCT_PACKAGES += \
-	android.hardware.power@1.1-service-qti \
-	android.hardware.power@1.1
+	android.hardware.power@1.1-service-qti
 
 # GFX
 PRODUCT_PACKAGES += \
